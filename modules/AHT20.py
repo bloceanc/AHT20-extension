8a9fbe0e432761a5415d1538dd49bc7bb97920dd
chenhy <chenhy@knownsec.com>
2024-06-19 21:35:24 +0800

    fix: 代码优化

--- modules/AHT20.py
+++ modules/AHT20.py
@@ -1,5 +1,5 @@
 from machine import Pin
-from machine import I2C
+from machine import SoftI2C
 from time import sleep
 import os
 from math import pow
@@ -7,12 +7,8 @@ from math import pow
 AHT20_ADDR = 0x38
 
 machine = os.uname().machine
-if ("KidBright32" in machine) or ("KidMotor V4" in machine):
-    i2c1 = I2C(1, scl=Pin(5), sda=Pin(4), freq=400000)
-elif "Mbits" in machine:
-    i2c1 = I2C(0, scl=Pin(21), sda=Pin(22), freq=400000)
-else:
-    i2c1 = I2C(0, scl=Pin(22), sda=Pin(21), freq=400000)
+
+i2c1 = SoftI2C(scl=Pin(22), sda=Pin(21), freq=400000)
    
 def read():
     try:
